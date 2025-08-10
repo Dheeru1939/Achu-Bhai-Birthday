f20f9436461da3f575a1132a73c50cd3650ba03a
Dheeraj Nair <96026754+Dheeru1939@users.noreply.github.com>
2025-08-11 00:21:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
-const app = require('../index');
+// api/index.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Example route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 module.exports = app;
